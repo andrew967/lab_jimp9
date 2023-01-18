57f0fc70379f480cc7e13a1c3d1280417b536c86
Andrew <andrew.venskij@bk.ru>
2023-01-18 18:56:30 +0100

    Backsubst files

---
+++ src/backsubst.c
@@ -0,0 +1,26 @@
+#include "backsubst.h"
+/**
+ * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
+ * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
+ * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
+ */
+int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
+
+    int n = mat -> r;
+    int m = b -> r;
+    if (n != m || mat->r != mat->c || b->c != 1) // jezeli mat nie jest macierza kwadratowa, lub liczba wierszy mat != liczbie werszy b, lub b ma wiecej niz 1 kolumne
+        return 2;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (mat->data[i][i] == 0)
+            return 1;
+        double ans = b->data[i][0];
+        for (int j = 0; j < n; j++)
+            if (i != j) ans -= mat->data[i][j] * x->data[j][0];
+        x->data[i][0] = ans / mat->data[i][i];
+    }
+
+    return 0;
+}
+
+
